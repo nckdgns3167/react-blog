b28d5a921b2474eba4578f3519d2b7a215d20e29
nckdgns3167 <nckdgns3167@naver.com>
2023-02-19 15:08:49 +0900

    블로그 생성 UI 구현 (기능 미구현)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const onSubmit = () => {
+    console.log(title, body);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="mb-2">
+        <label className="form-label">Title</label>
+        <input
+          className="form-control"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </div>
+      <div className="mb-2">
+        <label className="form-label">Body</label>
+        <textarea
+          className="form-control"
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+          rows="20"
+        />
+      </div>
+      <button className="btn btn-primary" onClick={onSubmit}>
+        Post
+      </button>
     </div>
   );
 }
