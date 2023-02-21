949a23c95b72bc35ae05aeebfa5740cd3469e756
nckdgns3167 <nckdgns3167@naver.com>
2023-02-21 23:21:50 +0900

    목록 불러오는 API 호출하며 useEffect를 사용하는 이유 경험

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const ListPage = () => {
-  return <div>ListPage</div>;
+  const [posts, setPosts] = useState([]);
+  const getPosts = () => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setPosts(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+      {posts.map((post) => {
+        return <div key={post.id}>{post.title}</div>;
+      })}
+    </div>
+  );
 };
 
 export default ListPage;
