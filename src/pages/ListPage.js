575ed2ff9fe8fe6dcba4b72761308c0bcd1f8c1f
nckdgns3167 <nckdgns3167@naver.com>
2023-02-23 00:26:10 +0900

    삼항연산자 남용으로 인한 가독성 저하를 개선함.

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import Card from "../components/Card";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const ListPage = () => {
   const history = useHistory();
@@ -19,12 +20,35 @@ const ListPage = () => {
     getPosts();
   }, []); // 한 번만 실행토록 빈 배열 걸어줌.
 
-  function deleteBlog(e, id) {
+  const deleteBlog = (e, id) => {
     e.stopPropagation();
     axios.delete(`http://localhost:3001/posts/${id}`).then(() => {
       setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
     });
-  }
+  };
+
+  const renderBlogList = () => {
+    if (loading) return <LoadingSpinner />;
+
+    if (posts.length === 0) return "게시물이 없습니다.";
+
+    return posts.map((post) => (
+      <Card
+        key={post.id}
+        title={post.title}
+        onClick={() => history.push("/blogs/edit")}
+      >
+        <div>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={(e) => deleteBlog(e, post.id)}
+          >
+            삭제
+          </button>
+        </div>
+      </Card>
+    ));
+  };
 
   return (
     <div>
@@ -36,34 +60,7 @@ const ListPage = () => {
           </Link>
         </div>
       </div>
-      {loading ? (
-        <div className="d-flex justify-content-center">
-          <div className="spinner-border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </div>
-        </div>
-      ) : posts.length > 0 ? (
-        posts.map((post) => {
-          return (
-            <Card
-              key={post.id}
-              title={post.title}
-              onClick={() => history.push("/blogs/edit")}
-            >
-              <div>
-                <button
-                  className="btn btn-danger btn-sm"
-                  onClick={(e) => deleteBlog(e, post.id)}
-                >
-                  삭제
-                </button>
-              </div>
-            </Card>
-          );
-        })
-      ) : (
-        "게시물이 없습니다."
-      )}
+      {renderBlogList()}
     </div>
   );
 };
