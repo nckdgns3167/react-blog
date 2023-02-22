d90ee23d9590a97c7986fb3bb002fe0840a2b348
nckdgns3167 <nckdgns3167@naver.com>
2023-02-23 00:13:43 +0900

    게시물 삭제 기능 추가, 게시물 없을 때 처리 (1)

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -17,37 +17,43 @@ const ListPage = () => {
     getPosts();
   }, []); // 한 번만 실행토록 빈 배열 걸어줌.
 
+  function deleteBlog(e, id) {
+    e.stopPropagation();
+    axios.delete(`http://localhost:3001/posts/${id}`).then(() => {
+      setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
+    });
+  }
+
   return (
     <div>
       <div className="d-flex justify-content-between">
         <h1>Blogs</h1>
         <div>
           <Link to="/blogs/create" className="btn btn-success">
-            POST 생성
+            게시물 생성
           </Link>
         </div>
       </div>
-      {posts.map((post) => {
-        return (
-          <Card
-            key={post.id}
-            title={post.title}
-            onClick={() => history.push("/blogs/edit")}
-          >
-            <div>
-              <button
-                className="btn btn-danger btn-sm"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  console.log("삭제");
-                }}
+      {posts.length > 0
+        ? posts.map((post) => {
+            return (
+              <Card
+                key={post.id}
+                title={post.title}
+                onClick={() => history.push("/blogs/edit")}
               >
-                삭제
-              </button>
-            </div>
-          </Card>
-        );
-      })}
+                <div>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={(e) => deleteBlog(e, post.id)}
+                  >
+                    삭제
+                  </button>
+                </div>
+              </Card>
+            );
+          })
+        : "게시물이 없습니다."}
     </div>
   );
 };
