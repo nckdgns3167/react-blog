8af42ed5f534d6ebd0ba99f3c02a6ad661fb4212
nckdgns3167 <nckdgns3167@naver.com>
2023-02-28 00:50:52 +0900

    페이지 prev, next, 버튼수 계산, useCallback, useLocation

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,25 +1,56 @@
 import axios from "axios";
 import { bool } from "prop-types";
-import React, { useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import React, { useCallback, useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import Card from "../components/Card";
 import LoadingSpinner from "../components/LoadingSpinner";
+import Pagination from "./Pagination";
 
 const BlogList = ({ isAdmin }) => {
   const history = useHistory();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const pageParam = params.get("_page");
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [numberOfPosts, setNumberOfPosts] = useState(0);
+  const [numberOfPages, setNumberOfPages] = useState(0);
+  const _limit = 1;
 
-  const getPosts = () => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      setPosts(response.data);
-      setLoading(false);
-    });
+  const onClickPageButton = (_page) => {
+    history.push(`${location.pathname}?_page=${_page}`);
+    getPosts(_page);
   };
 
+  const getPosts = useCallback(
+    (_page = 1, _sort = "id", _order = "desc") => {
+      let params = {
+        _page,
+        _limit,
+        _sort,
+        _order,
+      };
+
+      if (!isAdmin) params = { ...params, publish: true };
+
+      axios.get("http://localhost:3001/posts", { params }).then((response) => {
+        setNumberOfPosts(Number(response.headers["x-total-count"]));
+        setPosts(response.data);
+        setLoading(false);
+      });
+    },
+    [isAdmin]
+  );
+
   useEffect(() => {
-    getPosts();
-  }, []); // 한 번만 실행토록 빈 배열 걸어줌.
+    setNumberOfPages(Math.ceil(numberOfPosts / _limit));
+  }, [numberOfPosts]);
+
+  useEffect(() => {
+    setCurrentPage(parseInt(pageParam) || 1);
+    getPosts(parseInt(pageParam) || 1);
+  }, [pageParam, getPosts]);
 
   const deleteBlog = (e, id) => {
     e.stopPropagation();
@@ -32,28 +63,45 @@ const BlogList = ({ isAdmin }) => {
 
   if (posts.length === 0) return "게시물이 없습니다.";
 
-  return posts
-    .filter((post) => {
-      return isAdmin || post.publish;
-    })
-    .map((post) => (
-      <Card
-        key={post.id}
-        title={post.title}
-        onClick={() => history.push(`/blogs/${post.id}`)}
-      >
-        {isAdmin ? (
-          <div>
-            <button
-              className="btn btn-danger btn-sm"
-              onClick={(e) => deleteBlog(e, post.id)}
-            >
-              삭제
-            </button>
-          </div>
-        ) : null}
-      </Card>
-    ));
+  const renderBlogList = () => {
+    return (
+      posts
+        // .filter((post) => {
+        //   return isAdmin || post.publish;
+        // })
+        .map((post) => (
+          <Card
+            key={post.id}
+            title={post.title}
+            onClick={() => history.push(`/blogs/${post.id}`)}
+          >
+            {isAdmin ? (
+              <div>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={(e) => deleteBlog(e, post.id)}
+                >
+                  삭제
+                </button>
+              </div>
+            ) : null}
+          </Card>
+        ))
+    );
+  };
+
+  return (
+    <div>
+      {renderBlogList()}
+      {numberOfPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          numberOfPages={numberOfPages}
+          getPosts={onClickPageButton}
+        />
+      )}
+    </div>
+  );
 };
 
 BlogList.propTypes = {
